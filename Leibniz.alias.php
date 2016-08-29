9c6ddae05c26cd7a086a2b272880581adbd80859
altarion37 <soos.andreas@yahoo.ie>
2016-08-29 13:34:02 +0200

    Add files via upload

---
+++ Leibniz.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for Leibniz
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Altarion
+ */
+$specialPageAliases['en'] = array(
+	'Leibniz' => array( 'Leibniz' )
+);
+
+/** Deutsch
+ * @author Altarion
+ */
+$specialPageAliases['de'] = array(
+	'Leibniz' => array( 'Leibniz' )
+);
